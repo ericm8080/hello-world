689ea679ca3b6c13b24ae44c5acf48c95c1d7f65
Eric Mueller <emueller004@gmail.com>
2017-01-16 22:00:58 -0600

    test repo01

---
+++ PracticeForEric.java
@@ -0,0 +1,16 @@
+package stringPractice;
+
+public class PracticeForEric {
+
+	public static void main(String[] args) {
+		String name = new String("go to the store");
+		String name2 = "Target";
+		System.out.println(name);
+		String name3 = name.replaceAll("the", "2");
+		System.out.println("after:" + name3);
+		for(int i = 0,j=5;i<j;i++) {
+			System.out.println("test for e");
+		}
+	}
+
+}
